6b43ddcb708846fa9785de05e9f637044132daa1
Usman Qidwai <usmanqidwai21@gmail.com>
2023-08-26 19:14:51 -0400

    Arrays Completed

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Write your solution here!
+const cats = ['Milo','Otis','Garfield']
+
+
+function destructivelyAppendCat(name){
+  name = cats.push('Ralph')
+}
+
+function destructivelyPrependCat(name){
+  name = cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat(name){
+  name = cats.pop()
+}
+function destructivelyRemoveFirstCat(name){
+  name = cats.shift()
+}
+
+function appendCat(name){
+  return [...cats,name]
+ 
+
+
+
+
+
+}
+  
+function prependCat(name){
+  return [name,...cats]
+}
+function removeLastCat(){
+  return cats.slice(0,cats.length -1)
+  
+}
+ function removeFirstCat(){
+  return cats.slice(1)
+ }
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+// function prependCat(name){
+
+// }
